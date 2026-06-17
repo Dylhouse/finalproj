308e130c1ee613bd46877eba71337c6f1706b4fc
manlin0115 <mandyliu0115@gmail.com>
2026-06-17 11:28:46 -0700

    Create Classifier.h

---
+++ Classifier.h
@@ -0,0 +1,27 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Classifier
+{
+public:
+    static vector<vector<int>> getTemplateX();
+
+    static vector<vector<int>> getTemplateO();
+
+    static vector<vector<int>> getTemplateT();
+
+    static double distance(vector<int> v1, vector<int> v2);
+
+    static vector<int> flatten(const vector<vector<int>>& grid);
+
+    static string classify(const vector<vector<int>>& grid);
+
+    static double confidence(const vector<vector<int>>& grid);
+};
+
+#endif
